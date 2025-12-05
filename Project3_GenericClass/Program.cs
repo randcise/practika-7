12409f4f9a034b72d95f6593b2e8dc60351b77f2
рандкаис <o.yakymchuk@istu.edu.ua>
2025-12-05 13:27:50 +0200

    Add files via upload

---
+++ Project3_GenericClass/Program.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+
+public class OneType<T> where T : IComparable<T>
+{
+    private T value;
+
+    public OneType(T val)
+    {
+        value = val;
+    }
+
+    public T Smaller(T a, T b)
+    {
+        return a.CompareTo(b) < 0 ? a : b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        OneType<int> obj = new OneType<int>(10);
+        Console.WriteLine("Менше з 7 і 3 = " + obj.Smaller(7, 3));
+
+        OneType<double> obj2 = new OneType<double>(5.5);
+        Console.WriteLine("Менше з 2.5 і 4.7 = " + obj2.Smaller(2.5, 4.7));
+    }
+}
---
+++ Project4_GenericList/Program.cs
@@ -0,0 +1,47 @@
+﻿#nullable disable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Клас Друкарське Видання
+public class Publication
+{
+    public string Title { get; set; }
+    public int Pages { get; set; }
+
+    public Publication(string title, int pages)
+    {
+        Title = title;
+        Pages = pages;
+    }
+
+    public override string ToString() => $"{Title} ({Pages} сторінок)";
+}
+
+// Узагальнений список
+public class GenericList<T> : IEnumerable<T>
+{
+    private List<T> list = new List<T>();
+
+    public void Add(T item) => list.Add(item);
+
+    public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+class Program
+{
+    static void Main()
+    {
+        GenericList<Publication> publications = new GenericList<Publication>();
+
+        publications.Add(new Publication("Forbes", 80));
+        publications.Add(new Publication("National Geographic", 120));
+        publications.Add(new Publication("Science Weekly", 90));
+
+        Console.WriteLine("Список друкарських видань:");
+        foreach (var pub in publications)
+            Console.WriteLine(pub);
+    }
+}
