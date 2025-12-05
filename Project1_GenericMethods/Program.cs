88b522ddae201e5d8b97a611ea08085089cbda17
рандкаис <o.yakymchuk@istu.edu.ua>
2025-12-05 13:26:57 +0200

    Add files via upload

---
+++ Project1_GenericMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿#nullable disable
+using System;
+
+class Program
+{
+    // Універсальний метод обміну
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main()
+    {
+        double x = 3.14, y = 8.25;
+        Console.WriteLine($"До обміну: x={x}, y={y}");
+        Swap(ref x, ref y);
+        Console.WriteLine($"Після обміну: x={x}, y={y}");
+
+        string s1 = "Hello", s2 = "World";
+        Console.WriteLine($"\nДо обміну: {s1}, {s2}");
+        Swap(ref s1, ref s2);
+        Console.WriteLine($"Після обміну: {s1}, {s2}");
+    }
+}
---
+++ Project2_GenericConstraints/Program.cs
@@ -0,0 +1,32 @@
+﻿#nullable disable
+using System;
+
+class Program
+{
+    // max
+    public static T Max<T>(T[] arr) where T : IComparable<T>
+    {
+        T max = arr[0];
+        foreach (var x in arr)
+            if (x.CompareTo(max) > 0) max = x;
+        return max;
+    }
+
+    // min
+    public static T Min<T>(T[] arr) where T : IComparable<T>
+    {
+        T min = arr[0];
+        foreach (var x in arr)
+            if (x.CompareTo(min) < 0) min = x;
+        return min;
+    }
+
+    static void Main()
+    {
+        int[] a = { 5, 3, 8, 2, 9 };
+        double[] b = { 3.1, 7.4, 1.2, 9.9 };
+
+        Console.WriteLine($"int max = {Max(a)}, int min = {Min(a)}");
+        Console.WriteLine($"double max = {Max(b)}, double min = {Min(b)}");
+    }
+}
